c6f8bf111949d54b6f6ed52c50e98c827818681b
“Andrey <gordov.ar@gmail.com>
2021-12-24 12:43:49 +0500

    1.0.0

--- src/ts/__tests__/Cart.test.ts
+++ src/ts/__tests__/Cart.test.ts
@@ -1,7 +1,84 @@
 import Cart from '../service/Cart';
+import Book from '../domain/Book';
+import Movie from '../domain/Movie'
 
 test('new card should be empty', () => {
   const cart = new Cart();
 
   expect(cart.items.length).toBe(0);
 });
+
+test('add book', () => {
+  const cart = new Cart();
+  const book =  new Book(1001, 'War and Piece', 'Leo Tolstoy', 2000, 1225);
+  cart.add(book);
+  expect(cart.items).toEqual([book]);
+});
+
+test('add movie', () => {
+  const cart = new Cart();
+  const movie = new Movie(
+    1234, 
+    'The Avengers', 
+    2012, 
+    'USA', 
+    'Avengers Assemble!', 
+    ['fantastyc', 'action', 'fantasy', 'adventure'],
+    '137 min',
+    399);
+  cart.add(movie);
+  expect(cart.items).toEqual([movie]);
+});
+
+test('get total', () => {
+  const cart = new Cart();
+  const movie = new Movie(
+    1234, 
+    'The Avengers', 
+    2012, 
+    'USA', 
+    'Avengers Assemble!', 
+    ['fantastyc', 'action', 'fantasy', 'adventure'],
+    '137 min',
+    399);
+  const book =  new Book(1001, 'War and Piece', 'Leo Tolstoy', 1225, 2000);
+  cart.add(book);
+  cart.add(movie);
+  expect(cart.getTotal()).toBe(2399);
+});
+
+test('get total with discount', () => {
+  const cart = new Cart();
+  const movie = new Movie(
+    1234, 
+    'The Avengers', 
+    2012, 
+    'USA', 
+    'Avengers Assemble!', 
+    ['fantastyc', 'action', 'fantasy', 'adventure'],
+    '137 min',
+    399);
+  const book =  new Book(1001, 'War and Piece', 'Leo Tolstoy', 1225, 2000);
+  cart.add(book);
+  cart.add(movie);
+  expect(cart.getTotalWithDiscount(10)).toBe(2159.1);
+});
+
+test('delete item', () => {
+  const cart = new Cart();
+  const movie = new Movie(
+    1234, 
+    'The Avengers', 
+    2012, 
+    'USA', 
+    'Avengers Assemble!', 
+    ['fantastyc', 'action', 'fantasy', 'adventure'],
+    '137 min',
+    399);
+  const book =  new Book(1001, 'War and Piece', 'Leo Tolstoy', 1225, 2000);
+  cart.add(book);
+  cart.add(movie);
+  cart.deleteItem(1001);
+  expect(cart.items).toEqual([movie]);
+});
+
--- src/ts/service/Cart.ts
+++ src/ts/service/Cart.ts
@@ -10,4 +10,16 @@ export default class Cart {
     get items(): Buyable[] {
         return [...this._items]; 
     }
+
+    getTotal(): number {
+        return this._items.reduce((acc, el) => el.price + acc, 0);
+    }
+
+    getTotalWithDiscount(discount: number): number {
+        return ((100 - discount) / 100) * this.getTotal();
+    }
+
+    deleteItem(id: number): void {
+        this._items.splice(this._items.findIndex((item) => item.id === id) , 1);
+    }
 }
